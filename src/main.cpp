fb246e711e1ba8dd1f490ddce10a8a94187d6f19
Adrian <adrian@mxlinux.org>
2026-09-15 00:40:15 -0400

    Refuse to start without a graphical display session

--- src/main.cpp
+++ src/main.cpp
@@ -45,6 +45,13 @@ bool isUefi();
 
 int main(int argc, char *argv[])
 {
+    if (qEnvironmentVariableIsEmpty("DISPLAY") && qEnvironmentVariableIsEmpty("WAYLAND_DISPLAY")
+        && qEnvironmentVariableIsEmpty("QT_QPA_PLATFORM")) {
+        qWarning("uefi-manager: no display available (DISPLAY and WAYLAND_DISPLAY are both unset); "
+                "a graphical session is required to run this program.");
+        return EXIT_FAILURE;
+    }
+
     if (getuid() == 0) {
         qputenv("XDG_RUNTIME_DIR", "/run/user/0");
         qunsetenv("SESSION_MANAGER");
